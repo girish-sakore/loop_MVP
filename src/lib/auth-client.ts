eabee34abcb4a21a699c546f3472b4816f85e109
Girish <95904842+girish-sakore@users.noreply.github.com>
2026-09-04 16:38:10 +0000

    removed baseURL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,11 +2,6 @@ import { createAuthClient } from "better-auth/react";
 import { magicLinkClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.VERCEL_ENV === "production"
-        ? `https://${process.env.VERCEL_URL}`
-        : process.env.VERCEL_ENV === "preview"
-            ? `https://${process.env.VERCEL_BRANCH_URL}`
-            : `http://localhost:3000`,
     plugins: [
         magicLinkClient()
     ]
