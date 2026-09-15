c00a94227d75921818363fb9ef8d133ec8c34858
Shubham-Gathe <shubhamgate383@gmail.com>
2026-09-15 15:19:06 +0530

    - Added google auth.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -33,6 +33,12 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
 
   // Expose extra user fields on the session object
   user: {
